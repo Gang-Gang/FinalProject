44d82c2163417a5fa63f7a33891487316d85fb13
smcpeek <38501238+smcpeek@users.noreply.github.com>
2018-04-27 09:32:25 -0500

    Added basic player movement

--- FightingGame/FightingGame/FightingGame/Fighter.cs
+++ FightingGame/FightingGame/FightingGame/Fighter.cs
@@ -2,33 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 namespace FightingGame
 {
     class Fighter
     {
-        public String name;
-        public Texture2D[] movementAnimations;
-        public int health;
-        public Move[] moveset;
-        public int speed;
-        //hitboxes+hurtboxes
-        //armor? maybe later
-
-        public Fighter(String n, Texture2D[] a, int h, Move[] m, int s)
+        public Fighter(int playerNum, int charNum)
         {
-            name = n;
-            movementAnimations = a;
-            health = h;
-            moveset = m;
-            speed = s;
+
         }
+
     }
 }
--- FightingGame/FightingGame/FightingGame/Game1.cs
+++ FightingGame/FightingGame/FightingGame/Game1.cs
@@ -18,16 +18,15 @@ namespace FightingGame
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Texture2D titleScreen;
-        Rectangle fullScreen;
+        Rectangle p1Rect;
+        Rectangle p2Rect;
+        PlayerIndex p1 = PlayerIndex.One;
+        PlayerIndex p2 = PlayerIndex.Two;
+        Texture2D texture;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
-            Window.AllowUserResizing = true;
-            graphics.PreferredBackBufferHeight = 1010;
-            graphics.PreferredBackBufferWidth = 1920;
-            graphics.ApplyChanges();
             Content.RootDirectory = "Content";
         }
 
@@ -40,7 +39,8 @@ namespace FightingGame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            fullScreen = new Rectangle(0, 0, 1920, 1080);
+            p1Rect = new Rectangle(0, 0, 50, 150);
+            p2Rect = new Rectangle(500, 0, 50, 150);
             base.Initialize();
         }
 
@@ -52,8 +52,9 @@ namespace FightingGame
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            titleScreen = this.Content.Load<Texture2D>("Screens/wipTitleScreen");
+
             // TODO: use this.Content to load your game content here
+            texture = Content.Load<Texture2D>("White square");
         }
 
         /// <summary>
@@ -77,10 +78,32 @@ namespace FightingGame
                 this.Exit();
 
             // TODO: Add your update logic here
+            Input();
 
             base.Update(gameTime);
         }
 
+        public void Input()
+        {
+            GamePadState p1CurrentState = GamePad.GetState(p1);
+            GamePadState p2CurrentState = GamePad.GetState(p2);
+            if(p1CurrentState.ThumbSticks.Left.X < 0)
+            {
+                p1Rect.X += -3;
+            }
+            if (p1CurrentState.ThumbSticks.Left.X > 0)
+            {
+                p1Rect.X += 3;
+            }
+            if (p2CurrentState.ThumbSticks.Left.X < 0)
+            {
+                p2Rect.X += -3;
+            }
+            if (p2CurrentState.ThumbSticks.Left.X > 0)
+            {
+                p2Rect.X += 3;
+            }
+        }
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
@@ -91,7 +114,8 @@ namespace FightingGame
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            spriteBatch.Draw(titleScreen, fullScreen, Color.White);
+            spriteBatch.Draw(texture, p1Rect, Color.White);
+            spriteBatch.Draw(texture, p2Rect, Color.White);
             spriteBatch.End();
             base.Draw(gameTime);
         }

33c3a26bc8db000240082b274c16609c878899a5
Slyck17 <34579380+Slyck17@users.noreply.github.com>
2018-05-09 09:24:33 -0500

    Add files via upload (#11)

---
+++ FinalProject/FightingGame/FightingGame/FightingGame/Move.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace FightingGame
+{
+    class Move
+    {
+        Texture2D[] animations;
+        String name;
+        int damage;
+
+        public Move(Texture2D[] a, String n, int d)
+        {
+            animations = a;
+            name = n;
+            damage = d;
+        }
+    }
+}

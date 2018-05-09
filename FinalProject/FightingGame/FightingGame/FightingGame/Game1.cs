48d91624228fbdb7b836ba61083985b093775e9d
Slyck17 <34579380+Slyck17@users.noreply.github.com>
2018-05-09 09:21:45 -0500

    Add files via upload

---
+++ FinalProject/FightingGame/FightingGame/FightingGame/Game1.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace FightingGame
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D titleScreen;
+        Rectangle fullScreen;
+        Rectangle p1Rect;
+        Rectangle p2Rect;
+        PlayerIndex p1 = PlayerIndex.One;
+        PlayerIndex p2 = PlayerIndex.Two;
+        Texture2D texture;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Window.AllowUserResizing = true;
+            graphics.PreferredBackBufferHeight = 1010;
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.ApplyChanges();
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            p1Rect = new Rectangle(0, 0, 50, 150);
+            p2Rect = new Rectangle(500, 0, 50, 150);
+            fullScreen = new Rectangle(0, 0, 1920, 1080);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            titleScreen = this.Content.Load<Texture2D>("Screens/wipTitleScreen");
+            // TODO: use this.Content to load your game content here
+            texture = Content.Load<Texture2D>("White square");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+            Input();
+
+            base.Update(gameTime);
+        }
+
+        public void Input()
+        {
+            GamePadState p1CurrentState = GamePad.GetState(p1);
+            GamePadState p2CurrentState = GamePad.GetState(p2);
+            if(p1CurrentState.ThumbSticks.Left.X < 0)
+            {
+                p1Rect.X += -3;
+            }
+            if (p1CurrentState.ThumbSticks.Left.X > 0)
+            {
+                p1Rect.X += 3;
+            }
+            if (p2CurrentState.ThumbSticks.Left.X < 0)
+            {
+                p2Rect.X += -3;
+            }
+            if (p2CurrentState.ThumbSticks.Left.X > 0)
+            {
+                p2Rect.X += 3;
+            }
+        }
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(titleScreen, fullScreen, Color.White);
+            spriteBatch.Draw(texture, p1Rect, Color.White);
+            spriteBatch.Draw(texture, p2Rect, Color.White);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
